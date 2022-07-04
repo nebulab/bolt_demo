ff9c32f20ba274aa1330c8dec52e51556546b1ee
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2022-07-04 23:14:57 +0200

    Update solidus_schema after bundle update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_000832) do
+ActiveRecord::Schema.define(version: 2022_07_01_000926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2022_06_04_000832) do
     t.string "name", null: false
     t.text "body"
     t.string "record_type", null: false
-    t.integer "record_id", null: false
+    t.bigint "record_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2022_06_04_000832) do
   end
 
   create_table "solidus_bolt_bolt_configurations", force: :cascade do |t|
-    t.string "bearer_token"
     t.string "merchant_public_id"
     t.string "division_public_id"
     t.string "api_key"
